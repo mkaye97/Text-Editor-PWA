9f85df720567329506706150f1e91d5cf1444f79
mkaye <mattkaye97@gmail.com>
2023-04-30 17:17:42 -0400

    Added CssExtractPlugin to imports and client side package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 const { GenerateSW } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
